d87b26805b02db6189a046c5630c0cf6ae22e533
rafzal2020 <69915472+rafzal2020@users.noreply.github.com>
2020-10-22 12:32:50 -0400

    Update wlr.py

--- WLRbot/wlr.py
+++ WLRbot/wlr.py
@@ -16,8 +16,8 @@ async def on_ready():
 async def wlr(ctx):
 	await ctx.send(f"It has been {delta.days} days since Whole Lotta Red was announced.")
 	embed = discord.Embed(color = discord.Colour.red())
-	embed.set_image(url = "https://i.kym-cdn.com/photos/images/original/001/096/564/2f7.jpg")
+	embed.set_image(url = "ANY IMAGE HERE")
 	await ctx.send(embed = embed)
 
-client.run("NzY4ODU4MDI0ODAxODYxNjMy.X5Gk1Q.NiaKaZc7zHQOT5K3S3B7F3cXiNs")
+client.run("TOKEN")
 
