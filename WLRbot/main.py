e641868d9ac62cd4fb058a721bcb5ace274c8377
rafzal2020 <69915472+rafzal2020@users.noreply.github.com>
2020-10-22 21:26:52 -0400

    Update main.py
    line 67: Replace TOKEN with the API code given by the discord developing tool for the bot to work.

--- WLRbot/main.py
+++ WLRbot/main.py
@@ -4,53 +4,64 @@ from discord.ext import commands
 import random
 from datetime import date
 
-songs = []
-# empty list where user inputted songs will go
+client = commands.Bot(command_prefix='!')
 
 d0 = date(2018, 8, 23)
 d_today = date.today()
 delta = d_today - d0
-# calculates the amount of days since wlr was announced
-client = commands.Bot(command_prefix='!')
-
+songs = []
 
 @client.event
 async def on_ready():
     print("The bot is running.")
-    #notifies the bot is running in the console
 
+
+@client.command(aliases=['com'])
+async def commands(ctx):
+	command_list = open("commands.txt", "r")
+	file_contents = command_list.read()
+	await ctx.send(file_contents)
+	command_list.close()
+
+	
 @client.command()
 async def wlr(ctx):
     await ctx.send(f"It has been {delta.days} days since Whole Lotta Red was announced.")
     embed = discord.Embed(color=discord.Colour.red())
     embed.set_image(url='https://i.kym-cdn.com/photos/images/original/001/096/564/2f7.jpg')
     await ctx.send(embed=embed)
-	# will tell you the exact amount of days since the announcement of wlr
-	# displays an image
 
 
 @client.command(aliases=["as"])
-async def add(ctx,*,song):
-    await ctx.send(songs.append(song))
-# this command will add a song to the empty list
+async def add(ctx, *, song):
+	song_name = songs.append(song)
+	await ctx.send(f"Adding {song}...")
 
 
 @client.command(aliases=['q'])
 async def queue(ctx):
-    await ctx.send(songs)
-# this command prints the list
+	for song in songs:
+		await ctx.send(song.title())
 
 
 @client.command(aliases=['s'])
 async def sotd(ctx):
     sotd_random = random.choice(songs)
-    await ctx.send(f"Today's song of the day is {sotd_random}.")
-# randomly chooses an item from the list and prints the message
+    await ctx.send(f"Today's song of the day is {sotd_random.title()}.")
 
-# THIS PART IS STILL WIP
-## async def daily_song():
-    ## wait asyncio.sleep(1)
-    ##await sotd()
 
+@client.command(aliases=['c'])
+async def clear(ctx):
+    await ctx.send("All songs have been cleared.")
+    await ctx.send(songs.clear())
+
+
+@client.command(aliases=['hm'])
+async def howmany(ctx):
+    if len(songs) == 0:
+        await ctx.send("**There are no songs found!** Please add a song by entering '!as (link to the song)'")
+    else:
+        await ctx.send(f"There are **{len(songs)}** songs in the list.")
+	
 
 client.run("TOKEN")
